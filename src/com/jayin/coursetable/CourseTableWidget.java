d3125b75ec9b226671c37f879332863c22768a7f
Jayin Ton <tonjayin@gmail.com>
2014-02-04 02:09:45 +0800

    first blood

---
+++ src/com/jayin/coursetable/CourseTableWidget.java
@@ -0,0 +1,21 @@
+package com.jayin.coursetable;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+
+public class CourseTableWidget extends AppWidgetProvider {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		super.onReceive(context, intent);
+	}
+
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+			int[] appWidgetIds) {
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+	}
+
+}
